2dd7269b9d0977f09a2824fa594355e60c50f4eb
dgsellas@gmail.com <dgsellas@gmail.com>
2023-05-01 18:59:18 -0500

    init2

---
+++ LearnOpenGL/src/main.cpp
@@ -0,0 +1,152 @@
+#include "pch.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shaders.h"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// Creates GLFW window context
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+	// if window fails to create terminate program
+	if (window == NULL)
+	{
+		std::cout << "Failed to Open Window :(" << std::endl;
+
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD :(" << std::endl;
+		return -1;
+	}
+
+	// build and compile our shader program
+	// ------------------------------------
+	// vertex shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	else
+	{
+		std::cout << "INFO: Vertex Shader Loaded" << std::endl;
+	}
+	// fragment shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	else
+	{
+		std::cout << "INFO: Fragment Shader Loaded" << std::endl;
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << infoLog << std::endl;
+	}
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vert[] = {
+		-0.5f,
+		-0.5f,
+		0.0f,
+		0.5f,
+		-0.5f,
+		0.0f,
+		-0.5f,
+		0.5f,
+		0.0f,
+	};
+
+	// gen new buffer id
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	// 0. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vert), vert, GL_STATIC_DRAW);
+	// 1. then set the vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	// 2. use our shader program when we want to render an object
+	glUseProgram(shaderProgram);
+	// 3. now draw the object
+	// someOpenGLFunctionThatDrawsOurTriangle();
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// clears screen at start of each frame
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		processInput(window);
+
+		// render commands here
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+// resize window callback
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Process input
+void processInput(GLFWwindow *window)
+{
+	// if escape exit application
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
---
+++ premake5.lua
@@ -0,0 +1,75 @@
+workspace "LearnOpenGL"
+	architecture "x64"
+	startproject "LearnOpenGL"
+	configurations {"Debug", "Release"}
+
+	outputdir = "%{cfg.buildcfg}/%{cfg.system}_%{cfg.architecture}/%{prj.name}"
+
+	-- Includes
+	IncludeDir = {}
+	-- IncludeDir["SpdLog"] = "LearnOpenGL/vendor/spdlog/include"
+	-- IncludeDir["imgui"] = "LearnOpenGL/vendor/imgui"
+	IncludeDir["glfw"] = "LearnOpenGL/vendor/glfw/include"
+	IncludeDir["glad"] = "LearnOpenGL/vendor/glad/include"
+
+	group "Dependancies"
+
+		include "LearnOpenGL/vendor/glfw"
+		include "LearnOpenGL/vendor/glad"
+
+	group ""
+
+	project "LearnOpenGL"
+		location "LearnOpenGL"
+		kind "WindowedApp"
+		language "C++"
+		cppdialect "C++17"
+
+		targetdir("%{wks.location}/bin/" .. outputdir)
+		objdir("%{wks.location}/bin-int/" .. outputdir)
+		debugdir("%{wks.location}/bin/" .. outputdir)
+
+		pchheader "pch.h"
+		pchsource "LearnOpenGL/src/pch.cpp"
+
+		files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+		defines {"_CRT_SECURE_NO_WARNINGS"}
+
+		includedirs {
+			"%{prj.name}/src",
+			"%{IncludeDir.glfw}",
+			"%{IncludeDir.glad}"
+		-- "%{IncludeDir.DirectX11}",
+		-- "%{IncludeDir.imgui}"
+		}
+
+		links {
+			"glfw",
+			"glad",
+			--"includes/libs/x64/%{cfg.buildcfg}/*"
+		}
+
+--		filter "system:windows"
+--		systemversion "latest"
+
+		filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+		filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+		-- filter { "files:**.hlsl" }
+		-- 	shadermodel "5.0"
+		-- 	flags("ExcludeFromBuild")
+		-- 	shaderobjectfileoutput("%{wks.location}/bin/"..outputdir.."/Shaders/%{file.basename}.cso")
+
+		-- filter { "files:**_p.hlsl" }
+		-- 	removeflags "ExcludeFromBuild"
+		-- 	shadertype "Pixel"
+
+		-- filter { "files:**_v.hlsl" }
+		-- 	removeflags "ExcludeFromBuild"
+		-- 	shadertype "Vertex"
